ccf6b0a4780003b0f28d178eeef01a0ee4b2a915
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-02-16 00:51:45 +0100

    Install and setup oh-vue-icons as a plugin

---
+++ plugins/ohVueIcon.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import OhVueIcon from 'oh-vue-icons/components/Icon'
+
+// import { FcBusinessContact } from 'oh-vue-icons/icons'
+// OhVueIcon.add([FcBusinessContact])
+
+Vue.component('v-icon', OhVueIcon)
