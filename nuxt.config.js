b5b2cf0cd63537038747f316beda5cf793d84e30
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-02-15 18:36:16 +0100

    Add: animejs as a plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/style/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/anime.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/anime.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import anime from 'animejs/lib/anime.es.js'
+Vue.prototype.$anime = anime
