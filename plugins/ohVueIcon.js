4caef16bb0c34f709e4f3bd3f25d4fa034a65eaa
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-02-16 16:51:13 +0100

    Update ohVueIcon.js plugin - import more icons

--- plugins/ohVueIcon.js
+++ plugins/ohVueIcon.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import OhVueIcon from 'oh-vue-icons/components/Icon'
 
-// import { FcBusinessContact } from 'oh-vue-icons/icons'
-// OhVueIcon.add([FcBusinessContact])
+import {
+  FaHome,
+  BiInfoCircleFill,
+  RiContactsBookFill,
+  LaBlogSolid,
+  BiFacebook,
+  BiTwitter,
+  BiInstagram,
+  BiGithub,
+  BiLinkedin,
+  SiNetlify,
+} from 'oh-vue-icons/icons'
+
+const icons = [
+  FaHome,
+  BiInfoCircleFill,
+  LaBlogSolid,
+  RiContactsBookFill,
+  BiFacebook,
+  BiTwitter,
+  BiInstagram,
+  BiGithub,
+  BiLinkedin,
+  SiNetlify,
+]
+
+OhVueIcon.add(icons)
 
 Vue.component('v-icon', OhVueIcon)
