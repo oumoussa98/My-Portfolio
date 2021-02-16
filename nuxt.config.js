8878552450e448d5c2a9aef6f62b23e97417ea93
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-02-16 16:49:58 +0100

    Update(plugins): Nuxt.config.js -add oh-vue-icon to the plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,10 @@ export default {
   css: ['@/assets/style/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/anime.js', mode: 'client' }],
+  plugins: [
+    { src: '~/plugins/anime.js', mode: 'client' },
+    { src: '~/plugins/ohVueIcon.js', mode: 'client' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
