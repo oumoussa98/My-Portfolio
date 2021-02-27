5f13142df5354ef5041369b28796ff7d961124fb
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2021-02-28 00:38:54 +0100

    add: blog page

---
+++ pages/blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="blog">
+    <h1>Working on it</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Oumoussa | Blog',
+    meta: [
+      {
+        name: 'description',
+        content:
+          'Welcome to my blog where i share my experience on building stuff and solving problems',
+      },
+    ],
+  },
+}
+</script>
+
+<style></style>
